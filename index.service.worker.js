a551364efcfa62a3d5e2624eb5e84aa02e091183
Dillon <maroonpride98@gmail.com>
2025-11-25 23:41:14 -0600

    Add cache-busting v2 - force browser refresh

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1764134449|39959518';
+const CACHE_VERSION = 'v2-1764135667';
 /** @type {string} */
 const CACHE_PREFIX = 'EventHive-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
