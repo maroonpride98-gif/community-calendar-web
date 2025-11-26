ec2693086e6ac7bc1042a4a15b514203b90a4cc2
maroonpride98-gif <maroonpride98@gmail.com>
2025-11-26 00:12:54 -0600

    Add debug logging + cache v4 - test admin login

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1764136271|47008852';
+const CACHE_VERSION = '1764137480|47058045';
 /** @type {string} */
 const CACHE_PREFIX = 'EventHive-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
